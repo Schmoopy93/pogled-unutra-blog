d2d4db076f2719370a9724195302e2d4c86a18f1
Marko Rajić <38230581+Schmoopy93@users.noreply.github.com>
2022-04-06 15:00:08 +0200

    added modal on main page , fixed nav bar collapse on click

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,19 +7,6 @@ import {  trigger,state,style, animate, transition } from '@angular/animations';
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
-  animations: [
-    trigger('collapse', [
-      state('open', style({
-        opacity: '1'
-      })),
-      state('closed',   style({
-        opacity: '0',
-        display: 'none',   
-      })),
-      transition('closed => open', animate('400ms ease-in')),
-      transition('open => closed', animate('100ms ease-out'))
-    ])
-  ]
 })
 export class NavbarComponent implements OnInit {
 
@@ -35,7 +22,6 @@ export class NavbarComponent implements OnInit {
       this.isLoggedIn = true;
       this.roles = this.tokenStorage.getUser().roles;
     }
-    this.onResize(window);
   }
 
   logout(): void {
@@ -45,29 +31,4 @@ export class NavbarComponent implements OnInit {
       window.location.reload();
     });
   }
-
-  @HostListener('window:resize', ['$event.target']) 
-  onResize(event) { 
-    if(event.innerWidth > 990){
-      //need to set this to 'open' for large screens to show up because of opacity in 'closed' animation.
-      this._isNavbarCollapsedAnim = 'open';
-        this.isNavbarCollapsed = true;
-    }else{
-      // comment this line if you don't want to collapse the navbar when window is resized.
-     // this._isNavbarCollapsedAnim = 'closed';
-    }
-  }
-    toggleNavbar(): void {
-      if(this.isNavbarCollapsed){
-          this._isNavbarCollapsedAnim = 'open';
-        this.isNavbarCollapsed = false;
-      } else {
-      this._isNavbarCollapsedAnim = 'closed';
-        this.isNavbarCollapsed = true;
-      }
-    }
-    get isNavbarCollapsedAnim() : string {
-      return this._isNavbarCollapsedAnim;
-    }
-  }
-
+}
