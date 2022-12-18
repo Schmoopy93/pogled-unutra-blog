c2310ee998b8d090e72b3e6402dd745d0be4e5ec
Marko Rajić <38230581+Schmoopy93@users.noreply.github.com>
2022-12-18 18:26:04 +0100

    pagination on friend list

--- src/app/followers-list/followers-list.component.ts
+++ src/app/followers-list/followers-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AnonymousSubject } from 'rxjs/internal/Subject';
+import { Followers } from '../models/followers';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -14,45 +16,88 @@ export class FollowersListComponent implements OnInit {
   firstname = '';
   page = 1;
   count = 0;
-  pageSize = 10;
-  pageSizes = [10, 20, 30];
+  pageSize = 6;
+  pageSizes = [6, 12, 18];
   sortedItems: any;
   countAll: any;
+  text = '';
+  followers: any;
+  currentFollower:any;
+  check : any;
 
   constructor(private authService: AuthService) { }
 
 
   ngOnInit(): void {
     this.currUser = JSON.parse(window.sessionStorage.getItem('auth-user')).id;
-    this.retrieveUsers();
+    this.retrieveFollowers();
+  }
+
+  setActiveFollower(follower: Followers, index: number): void {
+    this.currentFollower = follower;
+    this.currentIndex = index;
   }
 
   handlePageChange(event: number): void {
     this.page = event;
-    this.retrieveUsers();
+    this.retrieveFollowers();
   }
 
   handlePageSizeChange(event: any): void {
     this.pageSize = event.target.value;
     this.page = 1;
-    this.retrieveUsers();
+    this.retrieveFollowers();
   }
 
-  retrieveUsers(): void {
-    this.authService.getUsers()
+  retrieveFollowers(): void {
+    const params = this.getRequestParams(this.text, this.page, this.pageSize, this.currUser);
+    this.authService.getMyFollowers(params)
     .subscribe(
       response => {
-        const res   = response;
-        const allUsers = res['users'] || undefined;
-
-        const currentUserFollowers = allUsers.filter(user => user.id === this.currUser)[0].followers.map(el => el.followerId);
+        const { followers, totalItems } = response;
+        this.followers = followers;
+        this.count = totalItems;
+        //console.log(followers, "follow")
+        // for(let i =0; i < followers.length; i++){
+        //   this.check = this.followers[i].user;
+        //   console.log(this.check, "check")
+        // }
         
-        this.users = allUsers.filter(user => (currentUserFollowers.includes(user.id) && user.id !==this.currUser));
+      },
+      error => {
+        console.log(error);
+      });
 
-        },
-        error => {
-          console.log(error);
-        });
   }
+
+  getRequestParams(searchTitle: string, page: number, pageSize: number, userId: any): any {
+    let params: any = {};
+
+    if (searchTitle) {
+      params[`firstname`] = searchTitle;
+    }
+
+    if (page) {
+      params[`page`] = page - 1;
+    }
+
+    if (pageSize) {
+      params[`size`] = pageSize;
+    }
+    
+    if (userId) {
+      params[`userId`] = userId;
+    }
+
+    return params;
+  }
+
+  // compareAlphabeticallyAsc() : void {
+  //   this.check.sort((a, b) => a.firstname.localeCompare(b.firstname))
+  // }
+
+  // compareAlphabeticallyDesc(): void {
+  //   this.check.sort((a, b) => b.firstname.localeCompare(a.firstname))
+  // }
 
 }
